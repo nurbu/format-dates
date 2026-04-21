d92404f486e77ec35b1560aa4be0a8fd55391d29
Nurbu <lamanurbu2@gmail.com>
2026-04-21 14:35:22 -0400

    Finished formatting month/day/year

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        System.out.println(formatter.format(today));
+        
+    }
 }
